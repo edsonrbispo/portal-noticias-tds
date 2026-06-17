d122df70e5fc6f3bfb9db67d81e52108b19facf7
Edson Rodrigues <edsonrb.2008@gmail.com>
2026-06-17 19:35:58 -0300

    Tabela Admin Noticias

--- resources/views/admin/noticias/index.blade.php
+++ resources/views/admin/noticias/index.blade.php
@@ -8,9 +8,26 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    Lista de Notícias
+                <div class="p-6 text-gray-900 dark:text-gray-100 flex justify-between items-center">
+                    <h1 class="text-xl font-bold">Lista de Notícias</h1>
+                    <a href="#" class="bg-black text-white px-3 py-2 rounded">+ Nova Notícia</a>
                 </div>
+
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Título</th>
+                                <th>Resumo</th>
+                                <th>Publicação</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+
+
             </div>
         </div>
     </div>
